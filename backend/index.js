5d3f250c974536282ff2422ee6b4259789fa1584
rayssabuarque <brayssa17@gmail.com>
2025-12-25 21:04:20 -0300

    feat: criar novos endpoints do Notion

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import cors from "cors";
+import { config } from "dotenv";
 import express from "express";
+
+import { get_leitura, get_multiplas_leituras } from './services/notion/get_data.js';
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Configurar Notion
-// const notion = new Client({ auth: process.env.NOTION_SECRET });
+config(); // Carregando variáveis do ENV
 const NOTION_KEY = process.env.NOTION_KEY;
 const DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
@@ -14,7 +17,7 @@ app.get('/test', async(req, res) => {
     res.json({message: `Backend funcionando!`});
 })
 
-
+// Teste de conexão (notion db)
 app.get('/api/notion/test', async(req, res) => {
   try {
     
@@ -35,6 +38,72 @@ app.get('/api/notion/test', async(req, res) => {
   }
   }
 );
+
+// Informações sobre Múltiplas Leituras
+///////////////////////////////////////
+// Para pegar uma 'nova página', utilizar:
+// /api/notion/reads?start_cursor=2d4bbd5b-c2b8-802d-8edb-f959c2206a6c
+app.get('/api/notion/reads', async(req, res) => {
+    try {
+        // Puxando 'início' da lista do cursor
+        const startCursor = req.query.start_cursor;
+        
+        const requestBody = {
+            page_size: 16
+        };
+        
+        if (startCursor) {
+            requestBody.start_cursor = startCursor;
+        }
+
+        const response = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${NOTION_KEY}`,
+                'Notion-Version': '2022-06-28',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        const data = await response.json();
+        const treated_data = get_multiplas_leituras(data);
+        return res.json(treated_data); // A resposta conterá os N resultados, `has_more` e `next_cursor`
+    } catch (e){
+        res.json({message: `ERRO: ${e}`});
+    }
+})
+
+// Informações sobre Leitura
+app.get('/api/notion/read/:ISBN', async(req, res) => {
+    try {
+        const livro_ISBN = req.params.ISBN; // Puxando o identificador do livro do request
+
+        const response = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${NOTION_KEY}`,
+                'Notion-Version': '2022-06-28',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                filter: {
+                    property: "ISBN", // Filtro de Identificador do Livro da Leitura
+                    title: {
+                        equals: livro_ISBN
+                    }
+                }
+            })
+        });
+
+        // Puxando e tratando o resultado do Notion Database
+        const data = await response.json();
+        const treated_data = get_leitura(data, 0); 
+        return res.json(treated_data)
+    } catch (e){
+        res.json({message: `ERRO: ${e}`});
+    }
+})
 
 // Porta para desenvolvimento
 const PORT = process.env.PORT || 3001;
---
+++ backend/services/notion/get_data.js
@@ -0,0 +1,61 @@
+
+// Retorna objeto com informações sobre uma leitura
+const get_leitura = (data, multiplo) => {
+
+    // Verifica se data e data.properties existem
+    if (!data) {
+        console.error('Dados inválidos para get_leitura:', data);
+        return null;
+    }
+
+    // DETERMINA QUAL OBJETO USAR:
+    // 0 = Leitura única; 1 = Múltipla Leitura
+    let props, id;
+    if (multiplo === 1) {
+        props = data.properties;
+        id = data.id;
+    } else {
+        if (!data.results || data.results.length === 0) {
+            console.error('Nenhum resultado encontrado:', data.results); // Verifica se há resultados
+            return null;
+        }
+        props = data.results[0].properties;
+        id = data.results[0].id;
+    }
+    
+    const objeto_leitura = {
+        id: id,
+        ISBN: props.ISBN?.title?.[0]?.plain_text || null,
+        titulo: props.titulo?.rich_text?.[0]?.plain_text || null,
+        autor_id: props.autor_id?.rich_text?.[0]?.plain_text || null,
+        datas_conclusao: props.datas_conclusao?.rich_text?.[0]?.plain_text || null,
+        status_leitura: props.status_leitura?.select?.name || null,
+        avaliacao: props.avaliacao.number,
+        resenha: props.resenha?.rich_text?.[0]?.plain_text || null,
+        formato: props.formato?.select?.name || null,
+        idioma: props.idioma?.rich_text?.[0]?.plain_text || null,
+    }
+
+    return objeto_leitura
+}
+
+const get_multiplas_leituras = (data) => {
+
+    // Verifica se data e data.properties existem
+    if (!data || !data.results[0].properties) {
+        console.error('Dados inválidos para get_leitura:', data);
+        return null;
+    }
+
+    const multiplas_leituras = [];
+
+    for (let i = 0; i < data.results.length; i++) {
+        const objeto_leitura = get_leitura(data.results[i], 1);
+        multiplas_leituras.push(objeto_leitura);
+    }
+
+    return multiplas_leituras;
+}
+
+export { get_leitura, get_multiplas_leituras };
+
